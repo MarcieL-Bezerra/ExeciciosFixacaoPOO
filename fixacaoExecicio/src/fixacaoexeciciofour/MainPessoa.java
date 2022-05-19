2551ef341bec3416a4de3fa5028ba2f6c8f48492
Marciel Bezerra <marcielgestao2007.1@gmail.com>
2022-05-19 07:54:27 -0300

    Questoes 4,5,6

--- fixacaoExecicio/src/fixacaoexeciciofour/MainPessoa.java
+++ fixacaoExecicio/src/fixacaoexeciciofour/MainPessoa.java
@@ -11,25 +11,49 @@ public class MainPessoa {
 		Pessoa p3 = new Pessoa("Paulo", 17);
 		Pessoa p4 = new Pessoa("Jessica", 18);
 		int maisIdade;
-		
-	List<Pessoa> pessoas = new ArrayList<>();
-	
-	pessoas.add(p1);
-	pessoas.add(p2);
-	pessoas.add(p3);
-	pessoas.add(p4);
-	
-	
-	
-	maisIdade = pessoas.get(0).getIdade();
-	for (int i = 0; i < pessoas.size(); i++) {
-		if (pessoas.get(i).getIdade() > maisIdade) {
+
+		List<Pessoa> pessoas = new ArrayList<>();
+
+		pessoas.add(p1);
+		pessoas.add(p2);
+		pessoas.add(p3);
+		pessoas.add(p4);
+
+		System.out.println("\n______________________ Questao 4 __________________\n");
+		maisIdade = pessoas.get(0).getIdade();
+		for (int i = 0; i < pessoas.size(); i++) {
+			if (pessoas.get(i).getIdade() > maisIdade) {
 				maisIdade = pessoas.get(i).getIdade();
-				System.out.println("A pessoa mais velha é " + pessoas.get(i).getNome() + " com a idade de " + pessoas.get(i).getIdade());
-				
-				}
-		
+				System.out.println("A pessoa mais velha é " + pessoas.get(i).getNome() + " com a idade de "
+						+ pessoas.get(i).getIdade());
+
+			}
+
 		}
-	
+
+		System.out.println("\n______________________ Questao 5 __________________\n");
+		System.out.println("Lista Antes");
+		for (int i = 0; i < pessoas.size(); i++) {
+			System.out.println("Nome: " + pessoas.get(i).getNome() + ", idade: " + pessoas.get(i).getIdade());
+
+		}
+		System.out.println("\nLista Depois");
+		for (int i = 0; i < pessoas.size(); i++) {
+			if (pessoas.get(i).getIdade() < 18) {
+				pessoas.remove(i);
+			}
+			System.out.println("Nome: " + pessoas.get(i).getNome() + ", idade: " + pessoas.get(i).getIdade());
+		}
+		System.out.println("\n______________________ Questao 6 __________________\n");
+
+		for (int i = 0; i < pessoas.size(); i++) {
+			if (pessoas.get(i).getNome().equalsIgnoreCase("Jessica")) {
+				System.out.println("A Jessica existe na lista, idade: " + pessoas.get(i).getIdade());
+			}else if(i == pessoas.size()-1){
+				System.out.println("A Jessica não está na lista");
+			}
+			
+		}
+
 	}
 }
--- fixacaoExecicio/src/fixacaoexeciciotwomain/questaotwomain.java
+++ fixacaoExecicio/src/fixacaoexeciciotwomain/questaotwomain.java
@@ -11,7 +11,7 @@ public class questaotwomain {
 
 	public static void main(String[] args) {
 		
-		Livro l1 = new Livro("Harry Potter", 40.5, 50,"J. K. Rowling", "fantasia", 300);
+		Livro l1 = new Livro("Harry Potter", 40, 50,"J. K. Rowling", "fantasia", 300);
 		Livro l2 = new Livro("Senhor do Anéis", 60, 30,"J. R. R. Tolkien", "fantasia", 500);
 		Livro l3 = new Livro("Java POO", 20, 50, "GFT", "educativo", 500);
 		
